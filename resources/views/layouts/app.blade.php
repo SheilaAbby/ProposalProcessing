df9a777add47d1b1e1f663747357fc1916b7bddb
SheilaAbby <sheilaabigael2@gmail.com>
2018-09-28 22:44:10 +0300

    loggin.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,7 +107,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <script type="text/javascript" src="{{asset('js/myjs.js')}}"></script>
     <script type="text/javascript">
         function buttonDisable(){
